97431849bae3c1089b176dfbf13c187c4c394def
tejomaib <tejbuyyana@gmail.com>
2024-08-16 00:37:58 -0500

    basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Projects from './components/Projects';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
