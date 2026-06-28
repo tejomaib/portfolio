534bae71df12d93fc99fcc17b650a579e8c4fe92
tejomaib <tejbuyyana@gmail.com>
2026-06-27 21:48:33 -0700

    Remove unused Grid import; update lockfiles

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Grid, Box } from '@mui/material';
+import { Container, Typography, Box } from '@mui/material';
 
 function Resume() {
     return (
